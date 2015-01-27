d9576c653c1d6734123b211245bd145989dcce3a
John Eikenberry <jae@zhar.net>
2015-01-27 14:49:59 -0800

    rework log to allow package user to set log output

--- client/log/logging.go
+++ client/log/logging.go
@@ -1,23 +1,27 @@
-// +build release
-
 package log
 
 import (
 	"log"
 	"log/syslog"
+	"os"
 )
 
-var priority = syslog.LOG_WARNING | syslog.LOG_USER
-var flags = log.LstdFlags | log.Lshortfile
-
-// Log to syslog in production
-var lg, err = syslog.NewLogger(priority, flags)
-
+// by default, log to stderr
+var lg = log.New(os.Stderr, "", log.Ltime|log.Lshortfile)
 var Print = lg.Print
 var Fatal = lg.Fatal
 
-func init() {
+// log to syslog in production
+// set in 'release' file specified with tagging
+func Syslog() {
+	var priority = syslog.LOG_WARNING | syslog.LOG_USER
+	var flags = log.LstdFlags | log.Lshortfile
+
+	var lg, err = syslog.NewLogger(priority, flags)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	Print = lg.Print
+	Fatal = lg.Fatal
 }

9b1dcb424831f6d6dd0a25d6217f2dd0333abcc3
John Eikenberry <john.eikenberry@nike.com>
2015-03-02 19:14:13 -0800

    move client instance into metadata struct
    allows giving it mocked data for testing

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -13,37 +13,36 @@ type metadata struct {
 	ip      string
 	version string
 	path    string
+	client  *jaws.Client
 	cache   chan map[string]string
 }
 
 func New(ip, version, path string) *metadata {
 	cache := make(chan map[string]string, 1)
 	cache <- make(map[string]string)
-	return &metadata{ip: ip, version: version, cache: cache}
+	client := jaws.New(false)
+	return &metadata{ip, version, path, client, cache}
 }
 
 // Provide latest version as default.
 var Latest = New("169.254.169.254", "latest", "meta-data")
 var Lookup = Latest.Lookup
 
-// http client
-var client = jaws.New(false)
-
 // Build the URL
-func (md metadata) Url(key string) string {
+func (md *metadata) Url(key string) string {
 	return "http://" +
 		strings.Join([]string{md.ip, md.version, md.path, key}, "/")
 }
 
 // Get one value from metadata (with key with meta-data/ as root)
-func (md metadata) Lookup(key string) (string, error) {
+func (md *metadata) Lookup(key string) (string, error) {
 	c := <-md.cache
 	defer func() { md.cache <- c }()
 	if v, ok := c[key]; ok {
 		return v, nil
 	}
 
-	r, err := client.Get(md.Url(key))
+	r, err := md.client.Get(md.Url(key))
 	var body []byte
 	if err == nil {
 		body, err = jaws.ReadBody(r)
@@ -53,4 +52,11 @@ func (md metadata) Lookup(key string) (string, error) {
 	} // no metadata == bad
 	c[key] = string(body)
 	return c[key], nil
+}
+
+func (md *metadata) Mock(replies ...jaws.Reply) {
+	if len(replies) == 1 {
+		md.client.Cli = replies[0]
+	}
+	md.client.Cli = jaws.NewReplies(replies...)
 }
--- metadata/metadata_test.go
+++ metadata/metadata_test.go
@@ -8,22 +8,22 @@ import (
 )
 
 func TestLookup(t *testing.T) {
-	client.Cli = jaws.Reply{Status: 200, Body: []byte("us-west-2a")}
+	Latest.Mock(jaws.Reply{Status: 200, Body: []byte("us-west-2a")})
 	val, err := Lookup("placement/availability-zone")
 	assert.Nil(t, err)
 	assert.Equal(t, val, "us-west-2a")
 	// test cache
-	client.Cli = jaws.Reply{Status: 200, Body: []byte("")}
+	Latest.Mock(jaws.Reply{Status: 200, Body: []byte("")})
 	val, err = Lookup("placement/availability-zone")
 	assert.Nil(t, err)
 	assert.Equal(t, val, "us-west-2a")
 }
 
 func TestCache(t *testing.T) {
-	client.Cli = jaws.Reply{Status: 200, Body: []byte("us-west-2a")}
+	Latest.Mock(jaws.Reply{Status: 200, Body: []byte("us-west-2a")})
 	Lookup("placement/availability-zone") // prime cache
 	// empty Get to fail if cache doesn't work
-	client.Cli = jaws.Reply{Status: 200, Body: []byte("")}
+	Latest.Mock(jaws.Reply{Status: 200, Body: []byte("")})
 	val, err := Lookup("placement/availability-zone")
 	assert.Nil(t, err)
 	assert.Equal(t, val, "us-west-2a")
